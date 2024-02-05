b89df0899c77805a81533d7ce2e53683dd65855a
Andrey Trubnikov <anteii3mer@gmail.com>
2024-02-05 15:16:36 +0400

    Add example of generic (python 12)

--- Part III - Classes and Protocols/Chapter 13 - Interfaces, Protocols, and ABCs/example13-13.py
+++ Part III - Classes and Protocols/Chapter 13 - Interfaces, Protocols, and ABCs/example13-13.py
@@ -15,9 +15,14 @@ class Repeatable(Protocol):
 
 @runtime_checkable
 class AlternativeRepeatable(Protocol):
-    # Changed signature from (self: T, repeat_count: int) to this
-    # Pylance is triggered by absent of / argument in the end
-    def __mul__(self, repeat_count: int, /) -> 'AlternativeRepeatable': 
+    def __mul__(self: 'AlternativeRepeatable', 
+                repeat_count: int, /) -> 'AlternativeRepeatable': 
+        ...
+
+# Python 12
+@runtime_checkable
+class GenericRepeatable[Y](Protocol):
+    def __mul__(self: Y, repeat_count: int, /) -> Y: 
         ...
 
 RT = TypeVar('RT', bound=Repeatable)
@@ -31,8 +36,13 @@ def triple(x: Repeatable) -> Repeatable:
 def quadriple(x: AlternativeRepeatable) -> AlternativeRepeatable:
     return x * 4
 
+def quintiple(x: GenericRepeatable) -> GenericRepeatable:
+    return x * 5
+
 x = 5
 print(isinstance(x, Repeatable))
+print(isinstance(5, GenericRepeatable))
 double(x)       # OK
 triple(x)       # OK
 quadriple(x)    # OK
+quintiple(x)    # Why not OK?
